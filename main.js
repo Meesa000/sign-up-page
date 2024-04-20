ff91f406007b31a786b6167abf8986f71b920a72
Meesa000 <10476858579+Meesa000@users.noreply.github.com>
2024-04-20 22:43:06 +0100

    add password strength indicator

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 
-const usernameCharLimit = 3;
+const usernameCharLimit = 1;
+const passwordStrongLength = 4;
 const signUpBtn = document.getElementById('sign-up-btn').addEventListener('click', onSignUp);
 
 function onSignUp() {
+    
     const usernameInput = document.getElementById('username').value;
     const usernameLength = usernameInput.length;
-    console.log(usernameInput);
+    console.log('username: ' + usernameInput);
     const passwordInput = document.getElementById('password').value;
-    console.log(passwordInput);
+    console.log('password: ' + passwordInput);
     
-
-    if (usernameLength >= 3){
-        window.alert('Username exceeds the character limit of: ' + usernameCharLimit +'.');
+    
+    if (usernameLength < usernameCharLimit){
+        window.alert('Username is lower than the minimum character limit of: ' + usernameCharLimit +'.');
+        
     }
+
+    const passStrength = document.getElementById('pass-strength-container');
+    if (passwordInput.length > passwordStrongLength){
+        passStrength.textContent = 'Password Strength: STRONG';
+        passStrength.style.color = 'green';
+
+    }
+
+
+    // password strength
+    
 
 }
 
